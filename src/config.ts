48363777ec7b8c9c5cd6085ad7ad934552ebd604
quotawoosung <aaron@quotabook.com>
2020-09-11 22:47:02 +0900

    EDITOR key 바꿈

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,8 @@ export const Configs = {
   API_HOST: process.env.REACT_APP_API_HOST || 'http://localhost:8000',
   SENTRY: process.env.REACT_APP_SENTRY,
   DEBUG: process.env.REACT_APP_PROJECT === EProjectType.LOCAL,
-  EDITOR_API_KEY: process.env.EDITOR_API_KEY || 'gb33xv231s3fbp7qbgts3hee4wpuj8fx6truw5eo5l4hqvq6', // TinyMCE Editor 무료버젼
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || 'AIzaSyBXgBlI2Q9YaTBUCzHTqPFJHXY7r_P9qxY',
-  // google map
+  EDITOR_API_KEY: process.env.EDITOR_API_KEY || 'zrbvsgihjf35mjwwwcpdrqt2uwgom9mpj7vssxiopc5b3940', // TinyMCE Editor 무료버젼
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || 'AIzaSyBXgBlI2Q9YaTBUCzHTqPFJHXY7r_P9qxY', // google map
 };
 
 export default Configs;
